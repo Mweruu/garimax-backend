b5619796644915799b2c34fa5e3031b9ff569581
User <user@Users-MacBook-Pro.local>
2023-05-29 00:57:09 +0300

    add user registration

--- database/seeders/20230528025518-User.js
+++ database/seeders/20230528025518-User.js
@@ -1,3 +1,5 @@
+const bycrypt = require('bcryptjs');
+
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert(
     'Users',
@@ -7,7 +9,7 @@ module.exports = {
         lastName: 'Doe',
         email: 'janedoe@example.com',
         mobile: 0714151444,
-        password: '1234567',
+        password: bycrypt.hashSync('1234567', 10),
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -15,7 +17,7 @@ module.exports = {
         firstName: 'Jon',
         lastName: 'Die',
         mobile: 0715425667,
-        password: '1234567',
+        password: bycrypt.hashSync('1234567', 10),
         email: 'jondoe@example.com',
         createdAt: new Date(),
         updatedAt: new Date(),
