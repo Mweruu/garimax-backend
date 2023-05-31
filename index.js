5b18fe8416e694ce8f4e356f0c0130d9b0d23b58
Christine <mwangimweruc@gmail.com>
2023-05-31 16:07:03 +0300

    add vendors

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ db.sequelize.sync()
 // };
 // parse requests of content-type - application/json
 app.use(express.json());
+app.use(cors());
 app.use('/api', routes);
-// app.use(cors(corsOptions));
+
 
 
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -30,6 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to garimax application." });
+ 
 });
 
 // set port, listen for requests
