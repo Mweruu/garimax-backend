363e076d6fb7f108734ff61ab42317561f47bac0
User <user@Users-MacBook-Pro.local>
2023-06-01 09:11:15 +0300

    add vendord endpoints

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,10 @@ const controllers = require('../controllers')
 
 const router = Router();
 
-router.post('/users/register', controllers.registerUser);
-router.post('/users/login', controllers.loginUser);
-router.get('/users', controllers.getAllUsers);
+router.post('/users/register', controllers.userController.registerUser);
+router.post('/users/login', controllers.userController.loginUser);
+router.get('/users', controllers.userController.getAllUsers);
+router.post('/singleVendor/register', controllers.vendorController.registerSingleUser);
+router.post('/coVendor/register', controllers.vendorController.registerCoUser);
 
 module.exports = router;
